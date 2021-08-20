720cf37a8b726823a72fc3afa8922061b3903396
Kathysi <1820421932@qq.com>
2021-08-20 21:16:14 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -57,6 +57,7 @@ $(function () {
             //快速获取表单中的数据
             data: $(this).serialize(),
             success: function (res) {
+                console.log(res.message);
                 if (res.status !== 0) {
                     return layer.msg('登录失败！');
                 }
